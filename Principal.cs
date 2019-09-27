6399c8dc21f3c4d4347c6236e0942cc5de17280a
MiguelAngelPoo <55799100+MiguelAngelPoo@users.noreply.github.com>
2019-09-27 16:49:02 -0700

    Add files via upload

---
+++ Principal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversionConsola
+{
+    public class Principal
+    {
+        
+        
+       
+        public void capturardatos()
+        {
+
+            int totalcentavos;
+            int pesos;
+            int centavos;
+            
+            Console.WriteLine("ingese totalcentavos: ");
+            totalcentavos = Convert.ToInt32(Console.ReadLine());
+            
+
+            convertir(totalcentavos, out pesos, out centavos);
+            Console.WriteLine("los centavos son: "+centavos);
+            Console.WriteLine("los pesos son: " + pesos);
+            Console.ReadKey();
+
+        }
+
+        public void convertir(int totalcentavos, out int pesos, out int centavos)
+        {
+            pesos = totalcentavos / 100;
+            centavos = totalcentavos % 100;
+
+
+        }
+
+       
+    }
+}
